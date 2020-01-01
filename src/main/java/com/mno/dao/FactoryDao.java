802eed03dcf220c6c5b40b528890b26e818b4d62
Nannan <1041836312@qq.com>
2020-01-01 22:28:10 +0800

    项目初始化

---
+++ src/main/java/com/mno/dao/FactoryDao.java
@@ -0,0 +1,21 @@
+/**
+ * FileName: FactoryDao
+ * Author:   10418
+ * Date:     2020-01-01 22:19
+ * Description: dao工程类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 楠楠(Nannan))
+ */
+package com.mno.dao;
+
+/**
+ * DESC〈一句话功能简述〉<br>
+ * 〈dao工厂类〉
+ *
+ * @author 10418
+ * @create 2020-01-01
+ * @since 1.0.0
+ */
+public class FactoryDao {
+}
---
+++ src/main/java/com/mno/service/FactoryService.java
@@ -0,0 +1,21 @@
+/**
+ * FileName: FactoryService
+ * Author:   10418
+ * Date:     2020-01-01 22:12
+ * Description: 服务工厂类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 楠楠(Nannan))
+ */
+package com.mno.service;
+
+/**
+ * DESC〈一句话功能简述〉<br>
+ * 〈服务工厂类〉
+ *
+ * @author 10418
+ * @create 2020-01-01
+ * @since 1.0.0
+ */
+public class FactoryService {
+}
