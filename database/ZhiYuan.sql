d8e8de4ccd34cccf50ebb77d1e4dd97c6e060cdf
Nannan <1041836312@qq.com>
2020-01-02 11:42:01 +0800

    数据库文件

---
+++ database/ZhiYuan.sql
@@ -0,0 +1,64 @@
+-- --------------------------------------------------------
+-- 主机:                           192.168.74.129
+-- 服务器版本:                        8.0.18 - MySQL Community Server - GPL
+-- 服务器OS:                        Linux
+-- HeidiSQL 版本:                  10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for ZhiYuan
+DROP DATABASE IF EXISTS `ZhiYuan`;
+CREATE DATABASE IF NOT EXISTS `ZhiYuan` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ZhiYuan`;
+
+-- Dumping structure for table ZhiYuan.student
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `mathScore` int(11) NOT NULL DEFAULT '0',
+  `englishScore` int(11) NOT NULL DEFAULT '0',
+  `majorScore` int(11) NOT NULL DEFAULT '0',
+  `rank` int(11) NOT NULL DEFAULT '0',
+  `counter` int(11) NOT NULL DEFAULT '0',
+  `totalScore` int(11) NOT NULL DEFAULT '0',
+  `userId` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table ZhiYuan.student: ~0 rows (大约)
+DELETE FROM `student`;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+
+-- Dumping structure for table ZhiYuan.user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(16) NOT NULL DEFAULT '',
+  `username` varchar(16) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL DEFAULT '',
+  `role` varchar(10) NOT NULL DEFAULT 'student',
+  `remark` varchar(200) NOT NULL DEFAULT '',
+  `insertTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `loginName` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Dumping data for table ZhiYuan.user: ~0 rows (大约)
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `nickname`, `username`, `password`, `role`, `remark`, `insertTime`, `updateTime`) VALUES
+	(1, 'qqq', 'qqqq', 'qqqqq', 'admin', '', '2020-01-02 01:46:19', '2020-01-02 01:46:19'),
+	(2, 'asdasd', 'qqasdasdqq', 'qqqasdasqq', 'admin', '', '2020-01-02 01:48:35', '2020-01-02 01:48:35');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
