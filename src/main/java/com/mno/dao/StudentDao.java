9c543cc98618856bee645dc6392b22930eee2f0e
mao <mate.mao@outlook.com>
2020-01-02 14:55:17 +0800

    add comment

--- src/main/java/com/mno/dao/StudentDao.java
+++ src/main/java/com/mno/dao/StudentDao.java
@@ -1,7 +1,14 @@
 package com.mno.dao;
 
 import com.mno.model.Student;
-
+/**
+ * DESC〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author 10418
+ * @create 2020-01-02
+ * @since 1.0.0
+ */
 public interface StudentDao {
 
     Student getOneById(int id);
--- src/main/java/com/mno/dao/impl/StudentDaoImpl.java
+++ src/main/java/com/mno/dao/impl/StudentDaoImpl.java
@@ -4,6 +4,14 @@ import com.mno.dao.BaseDao;
 import com.mno.dao.StudentDao;
 import com.mno.model.Student;
 
+/**
+ * DESC〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author 10418
+ * @create 2020-01-02
+ * @since 1.0.0
+ */
 public class StudentDaoImpl extends BaseDao<Student> implements StudentDao {
     @Override
     public Student getOneById(int id) {
